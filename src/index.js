cada2e24b57148c11fecace1cb639c52937e5f68
Patricio Tapia <native.devil@gmail.com>
2017-05-23 16:05:58 -0400

    ADD: Youtube API Key

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = "AIzaSyDD0OIOG4cdYHkdyECuwTsLKD9aIbmZfSA";
+
 const App = () => <div>Hello!!!!!!!!!!</div>;
 
 ReactDOM.render( <App />, document.querySelector(".container") );

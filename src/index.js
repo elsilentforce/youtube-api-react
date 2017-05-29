ebda9191bf301bbf197288ac6f8b9b6c90eaf772
Patricio Tapia <native.devil@gmail.com>
2017-05-29 15:41:43 -0400

    ADD: Youtube api search test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// Import component
+// Import components
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+
 
 const API_KEY = "AIzaSyDD0OIOG4cdYHkdyECuwTsLKD9aIbmZfSA";
+
+YTSearch({ key: API_KEY, term: 'Huawei' }, data => console.log(data) );
 
 // Builds the complete App.
 const App = () => {

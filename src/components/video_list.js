449dffcbff935b872cf661f3d59190393670e559
Patricio Tapia <native.devil@gmail.com>
2017-05-30 15:41:18 -0400

    REFACT: Javascript code

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 import VideoListItem from './video_list_item'
 
 const VideoList = (props) => {
   // Creates an array of VideoList Items
   const VideoListItems = props.videos.map(
     (video) => {
-      return (<VideoListItem key={video.etag} video={video} />)
+      return <VideoListItem key={video.etag} video={video} />
     }
   )
 
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({ video }) => {
   const videoUrl = video.snippet.thumbnails.default.url
 
   return (

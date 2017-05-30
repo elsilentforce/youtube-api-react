8609a91298aabb8fe8acc7850f178772a6055309
Patricio Tapia <native.devil@gmail.com>
2017-05-30 15:04:52 -0400

    ADD: ESLinter config

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class SearchBar extends Component {
-  constructor(props){
-    super(props);
+  constructor(props) {
+    super(props)
     this.state = { term: '' }
   }
 
-  render(){
+  render() {
     return (
       <div>
-        <input onChange={ event => this.setState({ term: event.target.value }) } />
+        <input onChange={event => this.setState({ term: event.target.value })} />
       </div>
     )
   }
 }
 
-export default SearchBar;
+export default SearchBar
